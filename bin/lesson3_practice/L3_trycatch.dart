8c96411dccaeeacb887f6f4ccd8621654964b30c
bacng <nhb.fit.hut@gmail.com>
2022-06-13 06:34:58 +0700

    bt lesson 3

---
+++ bin/lesson3_practice/L3_trycatch.dart
@@ -0,0 +1,22 @@
+void main() {
+  checkTuoi();
+}
+
+
+void checkTuoi(){
+    /// List tuổi tác
+/// List: Kiểm tra các phần tử, nếu phần tử không hợp lệ thì đưa ra Exception
+///  thông Tuổi hợp kêh luôn dương, và không thể là số thập phân
+  var ageList = [1, 2, 3, 8,4 ,20, 25, 26, -30, 15.5, 40, 65];
+  for(var i = 0; i < ageList.length; i++){
+    try{
+      if(ageList[i] < 0 || ageList[i].runtimeType != int){
+        
+      print("Phần tử không hợp lệ: " + ageList[i].toString() + ", thứ tự thứ: ${i}");
+      }
+    }catch(e){
+      print(e);
+    }
+    
+  }
+}
